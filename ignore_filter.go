17837c2a321c6caa063d520310c73defc74de933
hirakiuc <hirakiuc@gmail.com>
2016-12-04 15:43:31 +0900

    Use git2go to support regexp in gitignore. (#7)
    * :sparkles: Use git2go to support regexp in gitignore.
    
    * :books: Add description about dependency

--- ignore_filter.go
+++ ignore_filter.go
@@ -1,37 +1,39 @@
 package main
 
-import (
-	"os"
-	"path/filepath"
-
-	"github.com/monochromegane/go-gitignore"
-)
+import git "github.com/libgit2/git2go"
 
 // IgnoreMatcher is an interface to check whether the target is match or not.
 type IgnoreMatcher interface {
 	Match(path string, isDir bool) bool
 }
 
-// NewIgnoreMatcher return new Matcher instance.
-func NewIgnoreMatcher(opts *Options) (IgnoreMatcher, error) {
-	return getIgnoreMatcher(opts.Path)
+type Matcher struct {
+	repo *git.Repository
 }
 
-func getIgnoreMatcher(path string) (gitignore.IgnoreMatcher, error) {
-	gitIgnoreFile, err := filepath.Rel(path, ".gitignore")
+// NewIgnoreMatcher return new Matcher instance.
+func NewIgnoreMatcher(opts *Options) (IgnoreMatcher, error) {
+	repo, err := git.OpenRepository(opts.Path)
 	if err != nil {
 		showError("%v\n", err)
 		return nil, err
 	}
 
-	if _, err := os.Stat(gitIgnoreFile); err != nil {
-		return nil, nil
+	return &Matcher{
+		repo,
+	}, nil
+}
+
+func (matcher *Matcher) Match(path string, isDir bool) bool {
+	if matcher.repo == nil {
+		return false
 	}
 
-	matcher, err := gitignore.NewGitIgnore(gitIgnoreFile)
+	ignored, err := matcher.repo.IsPathIgnored(path)
 	if err != nil {
 		showError("%v\n", err)
-		return nil, err
+		return false
 	}
-	return matcher, nil
+
+	return ignored
 }
